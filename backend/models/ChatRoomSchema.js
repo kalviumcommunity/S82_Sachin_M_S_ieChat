51fd96c8aa371c1e5e693b44f0d365ec58208651
Sachin-2006 <sachin06kaizen@gmail.com>
2025-04-05 11:53:11 +0530

    worked on Schema

---
+++ backend/models/ChatRoomSchema.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose")
+
+const ChatRoomSchema = new mongoose.Schema({
+    movie:{type:mongoose.Schema.Types.ObjectId, ref:"Movie",required:true},
+    messages:{
+        user:{types:mongoose.Schema.Types.ObjectId,ref:"User",required:true},
+        text:{type:String,required:true},
+        timestamp:{type:Date,default:Date.now()},
+    }
+})
+
+
+module.exports = mongoose.model("ChatRoom",ChatRoomSchema)
