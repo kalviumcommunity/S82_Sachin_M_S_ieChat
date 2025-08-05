d4ffbfe54b5307e14589db3864e22a8943d0fcbd
Sachin MS <72447101+laser069@users.noreply.github.com>
2025-08-05 11:34:51 +0530

    Update authController.js

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -63,6 +63,8 @@ module.exports.Login = async (req,res,next)=>{
 module.exports.Logout = async(req,res)=>{
     res.clearCookie("token",{
         httpOnly:true,
+        secure:true,
+        sameSite:"None",
     })
     res.json({message:"Logged out"})
 }
